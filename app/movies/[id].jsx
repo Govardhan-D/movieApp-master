885b9298a89adafc0015e81a95117855947e87f4
Govardhan-D <govardhan.d@neoito.com>
2025-05-26 17:26:28 +0530

    hhh

--- app/movies/[id].jsx
+++ app/movies/[id].jsx
@@ -75,6 +75,7 @@ export default function MovieInfo(){
                             }}
 
                             />
+                          
                         </View>
                     }
 
